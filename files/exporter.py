38b9bb592d29a1c6822a9752e53278d20a1bacee
Michael Gerber <m.gerber@senec.com>
2023-05-12 08:26:40 +0200

    Format files via black and fix flake8 warnings

--- files/exporter.py
+++ files/exporter.py
@@ -5,7 +5,6 @@ import argparse
 import http.server
 import os
 
-
 import requests
 
 
@@ -21,11 +20,13 @@ def _escape_strings(data: dict):
     :param data: python dictionary containing strings
     :return: dict which contains strings with correctly escaped quotes
     """
-    translate_map = str.maketrans({
-        "\\": "\\\\",
-        '"': '\\"',
-        "\n":  "\\n",
-    })
+    translate_map = str.maketrans(
+        {
+            "\\": "\\\\",
+            '"': '\\"',
+            "\n": "\\n",
+        }
+    )
 
     escape_dict = data
     for key in escape_dict:
@@ -37,99 +38,107 @@ def _escape_strings(data: dict):
 # Monitors
 def _fetch_paginated(offset, api_key):
     params = {
-        'api_key': api_key,
-        'format': 'json',
-        'response_times': 1,
-        'response_times_limit': 1,
-        'offset': offset,
+        "api_key": api_key,
+        "format": "json",
+        "response_times": 1,
+        "response_times_limit": 1,
+        "offset": offset,
     }
 
     return requests.post(
-        'https://api.uptimerobot.com/v2/getMonitors',
+        "https://api.uptimerobot.com/v2/getMonitors",
         data=params,
     ).json()
 
 
 def fetch_data(api_key):
-    monitors = {'monitors': []}
+    monitors = {"monitors": []}
     offset = 0
     response = _fetch_paginated(offset, api_key)
-    for monitor in response['monitors']:
-        monitors['monitors'].append(_escape_strings(monitor))
+    for monitor in response["monitors"]:
+        monitors["monitors"].append(_escape_strings(monitor))
 
-    while response['monitors']:
+    while response["monitors"]:
         offset = offset + 50
         response = _fetch_paginated(offset, api_key)
-        for monitor in response['monitors']:
-            monitors['monitors'].append(_escape_strings(monitor))
+        for monitor in response["monitors"]:
+            monitors["monitors"].append(_escape_strings(monitor))
     return monitors
 
 
 def format_prometheus(data):
-    result = ''
+    result = ""
     for item in data:
-        if item.get('status') == 0:
+        if item.get("status") == 0:
             value = 2
-        elif item.get('status') == 1:
+        elif item.get("status") == 1:
             value = 1
-        elif item.get('status') == 2:
+        elif item.get("status") == 2:
             value = 0
         else:
             value = 3
 
-        result += f'uptimerobot_status{{c1_name="{item.get("friendly_name")}",c2_url="{item.get("url")}"' \
-                  f',c3_type="{item.get("type")}",c4_sub_type="{item.get("sub_type")}",' \
-                  f'c5_keyword_type="{item.get("keyword_type")}",c6_keyword_value="{item.get("keyword_value")}",' \
-                  f'c7_http_username="{item.get("http_username")}",c8_port="{item.get("port")}",' \
-                  f'c9_interval="{item.get("interval")}"}} {value}\n'
+        result += (
+            f'uptimerobot_status{{c1_name="{item.get("friendly_name")}",c2_url="{item.get("url")}"'
+            f',c3_type="{item.get("type")}",c4_sub_type="{item.get("sub_type")}",'
+            f'c5_keyword_type="{item.get("keyword_type")}",c6_keyword_value="{item.get("keyword_value")}",'
+            f'c7_http_username="{item.get("http_username")}",c8_port="{item.get("port")}",'
+            f'c9_interval="{item.get("interval")}"}} {value}\n'
+        )
 
-        if item.get('status', 0) == 2 and item.get('response_times'):
-            result += f'uptimerobot_response_time{{name="{item.get("friendly_name")}",type="{item.get("type")}",' \
-                      f'url="{item.get("url")}"}} {item.get("response_times").pop().get("value")}\n'
+        if item.get("status", 0) == 2 and item.get("response_times"):
+            result += (
+                f'uptimerobot_response_time{{name="{item.get("friendly_name")}",type="{item.get("type")}",'
+                f'url="{item.get("url")}"}} {item.get("response_times").pop().get("value")}\n'
+            )
 
     return result
 
 
-## getAccountDetails
+# getAccountDetails
 def fetch_accountdetails(api_key):
     params = {
-        'api_key': api_key,
-        'format': 'json',
+        "api_key": api_key,
+        "format": "json",
     }
     req = requests.post(
-        'https://api.uptimerobot.com/v2/getAccountDetails',
+        "https://api.uptimerobot.com/v2/getAccountDetails",
         data=params,
     )
     return req.json()
 
 
 def format_prometheus_accountdetails(data):
-    result = f'uptimerobot_accountdetails{{name="{data["email"]}",monitor_limit="{data["monitor_limit"]}",' \
-             f'monitor_interval="{data["monitor_interval"]}",up_monitors="{data["up_monitors"]}",' \
-             f'down_monitors="{data["down_monitors"]}",paused_monitors="{data["paused_monitors"]}"}} 1\n'
+    result = (
+        f'uptimerobot_accountdetails{{name="{data["email"]}",monitor_limit="{data["monitor_limit"]}",'
+        f'monitor_interval="{data["monitor_interval"]}",up_monitors="{data["up_monitors"]}",'
+        f'down_monitors="{data["down_monitors"]}",paused_monitors="{data["paused_monitors"]}"}} 1\n'
+    )
 
     return result
 
 
-## public status pages
+# public status pages
 def fetch_psp(api_key):
     params = {
-        'api_key': api_key,
-        'format': 'json',
+        "api_key": api_key,
+        "format": "json",
     }
     req = requests.post(
-        'https://api.uptimerobot.com/v2/getPSPs',
+        "https://api.uptimerobot.com/v2/getPSPs",
         data=params,
     )
     return req.json()
 
 
 def format_prometheus_psp(data):
-    result = ''
+    result = ""
     for item in data:
-        result += f'uptimerobot_psp{{c1_name="{item.get("friendly_name")}",c2_custom_url="{item.get("custom_url")}",' \
-                  f'c3_standard_url="{item.get("standard_url")}",c4_monitors="{item.get("monitors")}",' \
-                  f'c5_sort="{item.get("sort")}"}} {item.get("status")}\n'
+        result += (
+            f'uptimerobot_psp{{c1_name="{item.get("friendly_name")}",c2_custom_url="{item.get("custom_url")}",'
+            f'c3_standard_url="{item.get("standard_url")}",c4_monitors="{item.get("monitors")}",'
+            f'c5_sort="{item.get("sort")}"}} {item.get("status")}\n'
+        )
     return result
 
 
@@ -141,41 +150,40 @@ class ReqHandler(http.server.BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
         self.end_headers()
+        self.wfile.write(format_prometheus(answer.get("monitors")).encode("utf-8"))
         self.wfile.write(
-            format_prometheus(answer.get('monitors')).encode('utf-8')
+            format_prometheus_accountdetails(accountdetails.get("account")).encode(
+                "utf-8"
+            )
         )
-        self.wfile.write(
-            format_prometheus_accountdetails(accountdetails.get('account')).encode('utf-8')
-        )
-        self.wfile.write(
-            format_prometheus_psp(psp.get('psps')).encode('utf-8')
-        )
+        self.wfile.write(format_prometheus_psp(psp.get("psps")).encode("utf-8"))
 
 
-if __name__ == '__main__':
-    if 'UPTIMEROBOT_API_KEY' in os.environ:
-        api_key = os.environ.get('UPTIMEROBOT_API_KEY')
-        server_name = os.environ.get('UPTIMEROBOT_SERVER_NAME', '0.0.0.0')
-        server_port = int(os.environ.get('UPTIMEROBOT_SERVER_PORT', '9705'))
+if __name__ == "__main__":
+    if "UPTIMEROBOT_API_KEY" in os.environ:
+        api_key = os.environ.get("UPTIMEROBOT_API_KEY")
+        server_name = os.environ.get("UPTIMEROBOT_SERVER_NAME", "0.0.0.0")
+        server_port = int(os.environ.get("UPTIMEROBOT_SERVER_PORT", "9705"))
     else:
         parser = argparse.ArgumentParser(
-            description='Export all check results from uptimerobot.txt'
-                        'for prometheus scraping.'
+            description="Export all check results from uptimerobot.txt"
+            "for prometheus scraping."
         )
         parser.add_argument(
-            'apikey',
-            help='Your uptimerobot.com API key. See account details.'
+            "apikey", help="Your uptimerobot.com API key. See account details."
         )
         parser.add_argument(
-            '--server_name', '-s',
-            default='0.0.0.0',
-            help='Server address to bind to.'
+            "--server_name",
+            "-s",
+            default="0.0.0.0",
+            help="Server address to bind to.",
         )
         parser.add_argument(
-            '--server_port', '-p',
+            "--server_port",
+            "-p",
             default=9705,
             type=int,
-            help='Port to bind to.'
+            help="Port to bind to.",
         )
         args = parser.parse_args()
         api_key = args.apikey
--- tests/exporter_test.py
+++ tests/exporter_test.py
@@ -2,33 +2,45 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+import sys
 import unittest
 
-import sys
-sys.path.append("files/")
 import exporter
 
-class Test(unittest.TestCase):
+sys.path.append("files/")
 
+
+class Test(unittest.TestCase):
     def test_escape_backslash(self):
-        self.assertEqual(exporter._escape_strings(
-            {'backslash': r'test\sdf'}),
-            {'backslash': r'test\\sdf'})
+        self.assertEqual(
+            exporter._escape_strings({"backslash": r"test\sdf"}),
+            {"backslash": r"test\\sdf"},
+        )
 
     def test_escape_quote(self):
-        self.assertEqual(exporter._escape_strings(
-            {'quote': '"test"'}),
-            {'quote': '\\"test\\"'})
+        self.assertEqual(
+            exporter._escape_strings({"quote": '"test"'}),
+            {"quote": '\\"test\\"'},
+        )
 
     def test_escape_newline(self):
-        self.assertEqual(exporter._escape_strings(
-            {'newline': 'test\ntest'}),
-            {'newline': r'test\ntest'})
+        self.assertEqual(
+            exporter._escape_strings({"newline": "test\ntest"}),
+            {"newline": r"test\ntest"},
+        )
 
     def test_escape_all_special_chars(self):
-        self.assertEqual(exporter._escape_strings(
-            {'all': 'One backslash \\ and one ASCII newline \n and one double-quote ".'}),
-            {'all': 'One backslash \\\\ and one ASCII newline \\n and one double-quote \\".'})
+        self.assertEqual(
+            exporter._escape_strings(
+                {
+                    "all": 'One backslash \\ and one ASCII newline \n and one double-quote ".'
+                }
+            ),
+            {
+                "all": 'One backslash \\\\ and one ASCII newline \\n and one double-quote \\".'
+            },
+        )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
